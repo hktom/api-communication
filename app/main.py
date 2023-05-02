04379df582d68d68c02ff030fc68e09abed645c1
Arnaud Wanet <andrewwanet9@gmail.com>
2023-05-02 13:36:40 +0100

    updated the main file

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ def home():
     return {"message": "Welcome to Axialis"}
 
 if __name__ == '__main__': 
-    app.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
